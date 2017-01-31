e96a4fbe0449be120c3ecfd94b65060838fa0d2c
Linus Lee <100479lee@gmail.com>
2017-01-31 17:10:05 -0500

    Disable serviceworker for now

--- js/main.js
+++ js/main.js
@@ -35,13 +35,13 @@ G3.init = function() {
     });
 
     // serviceWorker check
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('sw.js').then(function(registration) {
-            console.log('Service worker registration successful');
-        }).catch(function(err) {
-            console.error('Service worker registration failed', err);
-        });
-    }
+    // if ('serviceWorker' in navigator) {
+    //     navigator.serviceWorker.register('sw.js').then(function(registration) {
+    //         console.log('Service worker registration successful');
+    //     }).catch(function(err) {
+    //         console.error('Service worker registration failed', err);
+    //     });
+    // }
 
     console.info('G3 initialized');
 
