2b2f3c65e939152fdfaf47187f0052d75399f897
Sammidysam <sammidysam@gmail.com>
2017-03-01 23:51:14 -0500

    Simple window that can be closed

--- main.py
+++ main.py
@@ -1,8 +1,37 @@
 #! /usr/bin/env python
 
-import pygame
+import os
+import sys
+import pygame as pg
+
+class Control(object):
+    def __init__(self):
+        pg.init()
+        os.environ["SDL_VIDEO_CENTERED"] = "TRUE"
+        pg.display.set_caption("Grinch Gets Gifts")
+        self.screen = pg.display.set_mode((500, 500))
+        self.screen_rect = self.screen.get_rect()
+        self.done = False
+        self.keys = pg.key.get_pressed()
+        self.clock = pg.time.Clock()
+        self.fps = 60.0
+
+    def event_loop(self):
+        for event in pg.event.get():
+            self.keys = pg.key.get_pressed()
+            if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
+                self.done = True
+
+    def main_loop(self):
+        while not self.done:
+            self.event_loop()
+            self.screen.fill((50, 60, 50))
+            pg.display.update()
+            self.clock.tick(self.fps)
 
 def main():
-    pygame.init()
-    pygame.quit()
+    Control().main_loop()
+    pg.quit()
     sys.exit()
+
+main()
