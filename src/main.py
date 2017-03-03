4119a582524a16f692895c84586c9ee44512bda0
Sammidysam <sammidysam@gmail.com>
2017-03-03 13:56:57 -0500

    Use pygame instead of pg

--- src/main.py
+++ src/main.py
@@ -4,50 +4,52 @@ from enum import Enum
 
 import os
 import sys
-import pygame as pg
+
+import pygame
+
 from grinch import Grinch
 from box import Box
 from boxstack import BoxStack
 
 class Mode(Enum):
-        MENU = 0
-        INGAME = 1
-        GAMEOVER = 2
+    MENU = 0
+    INGAME = 1
+    GAMEOVER = 2
 
 class Control(object):
         def __init__(self):
-                pg.init()
-                os.environ["SDL_VIDEO_CENTERED"] = "TRUE"
-                pg.display.set_caption("Grinch Gets Gifts")
-                self.screen = pg.display.set_mode((0, 0), pg.FULLSCREEN)
-                self.screen_rect = self.screen.get_rect()
-                self.done = False
-                self.keys = pg.key.get_pressed()
-                self.clock = pg.time.Clock()
-                self.fps = 60.0
-                self.mode = Mode.MENU
-                self.grinch = Grinch(self.screen_rect.width,self.screen_rect.height)
-                self.boxstack = BoxStack()
-                self.fallrate = 2 # 100 pixels per second
+            pygame.init()
+            os.environ["SDL_VIDEO_CENTERED"] = "TRUE"
+            pygame.display.set_caption("Grinch Gets Gifts")
+            self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+            self.screen_rect = self.screen.get_rect()
+            self.done = False
+            self.keys = pygame.key.get_pressed()
+            self.clock = pygame.time.Clock()
+            self.fps = 60.0
+            self.mode = Mode.MENU
+            self.grinch = Grinch(self.screen_rect.width,self.screen_rect.height)
+            self.boxstack = BoxStack()
+            self.fallrate = 2 # 100 pixels per second
 
                 self.boxstack.boxes.append(Box(0,0,100,200))
 
-                pg.time.set_timer(pg.USEREVENT + 1, 20)
+                pygame.time.set_timer(pygame.USEREVENT + 1, 20)
 
         def event_loop(self):
-                for event in pg.event.get():
-                        self.keys = pg.key.get_pressed()
-                        if event.type == pg.QUIT or self.keys[pg.K_ESCAPE]:
-                                self.done = True
-                        if event.type == pg.USEREVENT + 1:
+                for event in pygame.event.get():
+                    self.keys = pygame.key.get_pressed()
+                        if event.type == pygame.QUIT or self.keys[pygame.K_ESCAPE]:
+                            self.done = True
+                        if event.type == pygame.USEREVENT + 1:
                                 for box in self.boxstack.boxes:
-                                        box.y += self.fallrate
+                                    box.y += self.fallrate
 
         def draw(self):
-                self.screen.fill((50, 60, 50))
+            self.screen.fill((50, 60, 50))
 
                 if self.mode == Mode.MENU:
-                        self.screen.fill((50, 0, 50))
+                    self.screen.fill((50, 0, 50))
 
                 self.screen.blit(self.grinch.image,[self.grinch.x,self.grinch.y])
 
@@ -56,18 +58,18 @@ class Control(object):
 
         def main_loop(self):
                 while not self.done:
-                        self.event_loop()
-                        self.draw()
-                        pg.display.update()
-                        self.clock.tick(self.fps)
+                    self.event_loop()
+                    self.draw()
+                    pygame.display.update()
+                    self.clock.tick(self.fps)
                         for box in self.boxstack.boxes:
                                 if box.y > self.screen_rect.height:
-                                        self.boxstack.boxes.pop()
-                                        print(len(self.boxstack.boxes))
+                                    self.boxstack.boxes.pop()
+                                    print(len(self.boxstack.boxes))
 
 def main():
-        Control().main_loop()
-        pg.quit()
-        sys.exit()
+    Control().main_loop()
+    pygame.quit()
+    sys.exit()
 
 main()
